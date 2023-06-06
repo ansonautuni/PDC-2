09c10178ebd977f19ffa59302fc65077fe26bfa8
vdn5587@autuni.ac.nz <133718965+ansonautuni@users.noreply.github.com>
2023-06-07 02:40:49 +1200

    added filepath class file to get the filepath

--- OneDrive/Documents/NetBeansProjects/JavaApplication1/src/Filepath.java
+++ OneDrive/Documents/NetBeansProjects/JavaApplication1/src/Filepath.java
@@ -3,6 +3,6 @@ public class Filepath
     public static String BalanceFilepath = "./src/balance.txt";
 
     public static String getBalanceFilepath() {
-        return "./src/balance.txt";
+        return "./src/balance.txt"; //path to balnce text file
     }
 }
